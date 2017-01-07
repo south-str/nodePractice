17b535b1f08ebe9c0a489607bd5699bb646c1320
strsouth <str@ANEMOS.local>
2017-01-07 14:41:15 +0900

    use promise

--- src/requestHandler/requestHandlers.js
+++ src/requestHandler/requestHandlers.js
@@ -3,28 +3,24 @@ const querystring = require('querystring'),
 
 function root(response){
   console.log(`Request handler 'root' was called.`);
-
-  /*
-  //let body = `<!DOCTYPE html><html><head><title>root</title><body><p>this is root</p>`;
-  let stream = fs.createReadStream(__dirname + '/../client/template/index.html');
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  stream.pipe(response);
-  response.end();
-  */
-  let body = fs.readFile(__dirname + '/../client/template/index.html', (err, data) => {
-    if(err){
-      console.log(err);
-    }else{
-      /*
-      response.writeHead(200, {'Content-Type': 'text/html'});
-      response.write(data);
-      response.end();
-      */
+  const promise = new Promise((resolve, reject) => {
+    fs.readFile(__dirname + '/../client/template/index.html', (err, data) => {
+      if(err){
+        reject(err);
+      }else{
+        resolve(data);
+      }
+    });
+  });
+  promise
+    .then(data => {
       response.statusCode = 200;
       response.setHeader('Content-Type', 'text/html');
       response.end(data);
-    }
-  });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 }
 
 exports.root = root;
